fa7c497759ce7782fc61462c9d87b8876b02928f
Thomas Carroll <thomas.carroll08@gmail.com>
2014-09-12 14:13:51 -0400

    Updates header & footer, & adds links to main.js&css

--- footer.php
+++ footer.php
@@ -1,87 +1,100 @@
 
 
 <footer class="for-the-page">
-  <div class="row">
-    <div class='medium-2 small-6 columns'>
-      <h5>
-        About
-      </h5>
-      <ul id="about-footer-links">
-        <li>
-          <a id="team-footer-link" data-destination-id="#team" href='#/about'>Team</a>
-        </li>
-        <li>
-          <a id="advisors-footer-link" data-destination-id="#advisors" href='#/about'>Advisors</a>
-        </li>
-        <li>
-          <a id="partners-footer-link" data-destination-id="#partners" href='#/about'>Partners</a>
-        </li>
-        <li>
-          <a id="press-footer-link" data-destination-id="#press" href='#/about'>Press</a>
-        </li>
-        <li>
-          <a href='https://recombine.com/careers'>Careers</a>
-        </li>
-      </ul>
-    </div>
-    <div class='medium-2 small-6 columns'>
-      <h5>
-        Info
-      </h5>
-      <ul>
-        <li>
-          <a href='https://patients.recombine.com'>Patients</a>
-        </li>
-        <li>
-          <a href='https://clinics.recombine.com'>Providers</a>
-        </li>
-        <li>
-          <a href='http://news.recombine.com'>News</a>
-        </li>
-      </ul>
-    </div>
-    <div class='medium-2 small-6 columns end'>
-      <h5>
-        Legal
-      </h5>
-      <ul>
-        <li>
-          <a href="#/privacy">Privacy</a>
-        </li>
-      </ul>
-    </div>
 
-    <div class='medium-6 columns text-right copyright-info'>
-      <ul class="no-bullet">
-        <li class="">
-          <a class='fi-comments contact-link' href='https://recombine.com/contact'>
-            Contact
-          </a>
-        </li>
-        <li class="">
-          <a
-            class='fi-social-twitter'
-            href='https://twitter.com/Recombine'>
-          </a>
-          <a
-            class='fi-social-facebook'
-            href='https://www.facebook.com/Recombine'>
-          </a>
-          <a
-            class='fi-social-google-plus'
-            href='https://plus.google.com/+Recombine/about'>
-          </a>
-          <a
-            class='fi-social-linkedin'
-            href='https://www.linkedin.com/company/recombine'>
-          </a>
-        </li>
-        <li class="copyright-notice">
-          &copy;&nbsp;2014&nbsp;Recombine
-        </li>
-      </ul>
-    </div>
-  </div>
+	<div class="row">
+		<div class='medium-2 small-6 columns'>
+			<a href="//recombine.com/about">
+				<h5>
+					About
+				</h5>
+			</a>
+			<ul id="about-footer-links">
+				<li>
+					<a id="team-footer-link" href='//recombine.com/about?scrollTo=team'>Team</a>
+				</li>
+				<li>
+					<a id="advisors-footer-link"
+						href='//recombine.com/about?scrollTo=advisors'>
+						Advisors
+					</a>
+				</li>
+				<li>
+					<a
+						id="partners-footer-link"
+						href='//recombine.com/about?scrollTo=partners'>
+						Partners
+					</a>
+				</li>
+				<li>
+					<a id="press-footer-link" href='//recombine.com/about?scrollTo=press'>Press</a>
+				</li>
+				<li>
+					<a href='//recombine.com/careers'>Careers</a>
+				</li>
+			</ul>
+		</div>
+		<div class='medium-2 small-6 columns'>
+			<h5>
+				Info
+			</h5>
+			<ul>
+				<li>
+					<a href='//recombine.com/carriermap'>CarrierMap</a>
+				</li>
+				<li>
+					<a href='//recombine.com/fertilitymap'>FertilityMap</a>
+				</li>
+				<li>
+					<a href='//recombine.com/providers'>Providers</a>
+				</li>
+				<li>
+					<a href='//news.recombine.com'>News</a>
+				</li>
+			</ul>
+		</div>
+		<div class='medium-2 small-6 columns end'>
+			<h5>
+				Legal
+			</h5>
+			<ul>
+				<li>
+					<a href="//recombine.com/privacy">Privacy</a>
+				</li>
+			</ul>
+		</div>
+
+		<div class='medium-6 columns text-right copyright-info'>
+			<ul class="no-bullet">
+				<li class="">
+					<a class='fi-comments contact-link' href='//recombine.com/contact'>
+						Contact
+					</a>
+				</li>
+				<li class="">
+					<a
+						class='fi-social-twitter'
+						href='//twitter.com/Recombine'>
+					</a>
+					<a
+						class='fi-social-facebook'
+						href='//www.facebook.com/Recombine'>
+					</a>
+					<a
+						class='fi-social-google-plus'
+						href='//plus.google.com/+Recombine/about'>
+					</a>
+					<a
+						class='fi-social-linkedin'
+						href='//www.linkedin.com/company/recombine'>
+					</a>
+				</li>
+				<li class="copyright-notice">
+					<span class="light">&copy;&nbsp;2014&nbsp;Recombine</span>
+				</li>
+			</ul>
+		</div>
+	</div>
 </footer>
 
 
--- header.php
+++ header.php
@@ -22,12 +22,14 @@
     } ?></title>
 
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/app.css" />
+    <link rel="stylesheet" href="//recombine-public-new-staging.herokuapp.com/styles/main.css" />
     <link href="<?php echo get_template_directory_uri(); ?>/css/foundation-icons/foundation-icons.css?body=1" media="all" rel="stylesheet" />
 
     <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/assets/img/icons/favicon.png">
     <?php wp_head(); ?>
     
-    <script type="text/javascript" src="http://code.jquery.com/jquery-2.1.1.js"></script>
+    <script type="text/javascript" src="//code.jquery.com/jquery-2.1.1.js"></script>
+    <script type="text/javascript" src="//recombine-public-new-staging.herokuapp.com/scripts/main.js"></script>
     
     <script type="text/javascript" src="//use.typekit.net/oda5kuu.js"></script>
     <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
@@ -38,116 +40,167 @@
 
   <?php do_action('foundationPress_layout_start'); ?>
 
-    <div class="navigation-bar-module">
-      <div class="row">
-        <div class="large-12 columns">
-          <div class="top-bar-wrapper fixed">
-            <nav
-              class="top-bar"
-              data-options="scrolltop: false"
-              data-topbar
-              id="nav-bar">
-              <ul class="title-area">
-                <li class="name">
-                  <h1>
-                    <a href="/">
-                      <img class="logo" src="https://recombine.com/assets/logo.png" alt="Recombine logo">
-                      Recombine
-                    </a>                  
-                  </h1>
+  <div class="navigation-bar-module" ng-controller="NavbarController">
+    <div class="row">
+      <div class="large-12 columns">
+        <div class="top-bar-wrapper fixed">
+          <nav
+            class="top-bar"
+            data-options="scrolltop: false"
+            data-topbar
+            id="nav-bar">
+            <ul class="title-area">
+              <li class="name">
+                <h1>
+                  <a href="//recombine.com/">
+                    <img class="logo" src="images/logo.png" alt="Recombine logo">
+                    Recombine
+                  </a>
+                </h1>
+              </li>
+              <li class="toggle-topbar menu-icon">
+                <a href="">
+                  <span>Menu</span>
+                </a>
+              </li>
+            </ul>
+
+            <section class="top-bar-section">
+              <ul class="left">
+                <li class="has-dropdown">
+                  <a href="" id="services-dropdown-button">
+                    <span>Our Tests</span>
+                  </a>
+                  <ul class="dropdown hide-for-large-up">
+                    <li>
+                      <a href="//recombine.com/carriermap">
+                        <span>CarrierMap</span>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="//recombine.com/chromomap">
+                        <span>ChromoMap</span>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="//recombine.com/providers">
+                        <span>For Providers</span>
+                      </a>
+                    </li>
+                  </ul>
                 </li>
-                <li class="toggle-topbar menu-icon">
-                  <a href="">
-                    <span>Menu</span>
+                <li class="has-dropdown">
+                  <a href="" id="research-dropdown-button">
+                    <span>Our Research</span>
+                  </a>
+                  <ul class="dropdown hide-for-large-up">
+                    <li>
+                      <a href="//recombine.com/fertilitymap">
+                        <span>FertilityMap</span>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="//recombine.com/clinical-studies">
+                        <span>Clinical Studies</span>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="//recombine.com/responsible-science">
+                        <span>Responsible Science</span>
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+              <ul class="right">
+                <li class="">
+                  <a href="//help.recombine.com/">
+                    <span class="navbar-help-link">Help</span>
+                  </a>
+                </li>
+                <li ng-show="contextualButton() === 'register'">
+                  <a href="//recombine.com/register">
+                    <span>Register</span>
+                  </a>
+                </li>
+                <li ng-show="contextualButton() === 'claim test'">
+                  <a href="//recombine.com/enter-code">
+                    <span>Claim Test</span>
+                  </a>
+                </li>
+                <li ng-show="contextualButton() === 'patients site'">
+                  <a ng-click="goToPatientsSite()">
+                    <span>Dashboard</span>
+                  </a>
+                </li>
+                <li ng-show="contextualButton() === 'clinics site'">
+                  <a href="//clinics.recombine.com/dashboard">
+                    <span>Dashboard</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="//recombine.com/login" ng-hide="loggedIn()">
+                    <span>Log In</span>
+                  </a>
+                </li>
+                <li>
+                  <a ng-click="signOut()" ng-show="loggedIn()">
+                    <span>Log Out</span>
                   </a>
                 </li>
               </ul>
-
-              <section class="top-bar-section">
-                <ul class="right">
-                  <li>
-                    <a href="#/register">
-                      Register
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#/login">
-                      Log In
-                    </a>
-                  </li>
-                </ul>
-                <ul class="left">
-                  <li class="has-dropdown">
-                    <a href="" id="services-dropdown-button">
-                      Services
-                    </a>
-                    <ul class="dropdown hide-for-large-up">
-                      <li>
-                        <a href="#/carriermap">CarrierMap</a>
-                      </li>
-                      <li>
-                        <a href="#/fertilitymap">FertilityMap</a>
-                      </li>
-                      <li>
-                        <a href="">LoremMap</a>
-                      </li>
-                    </ul>
-                  </li>
-                  <li class="has-dropdown">
-                    <a href="" id="my-recombine-dropdown-button">
-                      My Recombine
-                    </a>
-                    <ul class="dropdown hide-for-large-up">
-                      <li>
-                        <a href="#/register-for-carriermap">Register for CarrierMap</a>
-                      </li>
-                      <li>
-                        <a href="#/claim-test/choose">Claim My Test</a>
-                      </li>
-                      <li>
-                        <a href="">View My Results</a>
-                      </li>
-                    </ul>
-                  </li>
-                </ul>
-              </section>
-            </nav>
-          </div>
+            </section>
+          </nav>
         </div>
-      </div>
-
-      <div class="top-bar-wrapper top-bar-submenu-wrapper fixed closed show-for-large-up">
-        <section class="top-bar-section top-bar-horizontal-submenu closed"
-          data-button-id="services-dropdown-button">
-          <ul class="inline-list no-bullet">
-            <li>
-              <a href="#/carriermap">CarrierMap</a>
-            </li>
-            <li>
-              <a href="#/fertilitymap">FertilityMap</a>
-            </li>
-            <li>
-              <a href="">LoremMap</a>
-            </li>
-          </ul>        
-        </section>
-  
-        <section class="top-bar-section top-bar-horizontal-submenu closed"
-          data-button-id="my-recombine-dropdown-button">
-          <ul class="inline-list no-bullet">
-            <li>
-              <a href="#/register-for-carriermap">Register for CarrierMap</a>
-            </li>
-            <li>
-              <a href="#/claim-test/choose">Claim My Test</a>
-            </li>
-            <li>
-              <a href="">View My Results</a>
-            </li>
-          </ul>        
-        </section>
       </div>
     </div>
 
+    <div class="top-bar-wrapper top-bar-submenu-wrapper fixed closed show-for-large-up">
+      <section class="top-bar-section top-bar-horizontal-submenu closed"
+        data-button-id="services-dropdown-button">
+        <ul class="inline-list no-bullet">
+          <li>
+            <a href="//recombine.com/carriermap">
+              <span>CarrierMap</span>
+            </a>
+          </li>
+          <li>
+            <a href="//recombine.com/chromomap">
+              <span>ChromoMap</span>
+            </a>
+          </li>
+          <li>
+            <a href="//recombine.com/providers">
+              <span>For Providers</span>
+            </a>
+          </li>
+        </ul>
+      </section>
+
+      <section class="top-bar-section top-bar-horizontal-submenu closed" data-button-id="research-dropdown-button">
+        <ul class="inline-list no-bullet">
+          <li>
+            <a href="//recombine.com/fertilitymap">
+              <span>FertilityMap</span>
+            </a>
+          </li>
+          <li>
+            <a href="//recombine.com/clinical-studies">
+              <span>Clinical Studies</span>
+            </a>
+          </li>
+          <li>
+            <a href="//recombine.com/responsible-science">
+              <span>Responsible Science</span>
+            </a>
+          </li>
+        </ul>
+      </section>
+    </div>
+  </div>
+
+  <script type="text/javascript">
+    pages.navBar();
+  </script>
 
   <?php do_action('foundationPress_after_header'); ?>
